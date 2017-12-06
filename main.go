4b78e0b23223ae0034b0441e9af25173bfb014c1
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-12-06 11:21:45 +0900

    merge file

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/url"
 	"os"
+	"os/exec"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
@@ -82,5 +83,91 @@ func fetchTrending(language string) error {
 		repos = append(repos, repo)
 	})
 
+	return nil
+}
+
+func keybindings(g *gocui.Gui) error {
+	if err := g.SetKeybinding("", gocui.KeyArrowDown, gocui.ModNone, cursorDown); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("", gocui.KeyArrowUp, gocui.ModNone, cursorUp); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("", gocui.KeyEnter, gocui.ModNone, open); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		return err
+	}
+	return nil
+}
+
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	if v, err := g.SetView("main", int(0.2*float32(maxX)), -1, maxX, maxY); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Title = "GitHub Trending"
+		v.Highlight = true
+		v.SelBgColor = gocui.ColorGreen
+		v.SelFgColor = gocui.ColorBlack
+
+		for _, r := range repos {
+			fmt.Fprintln(v, "["+r.name+"] "+r.desc)
+		}
+	}
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
+
+func open(g *gocui.Gui, v *gocui.View) error {
+	var l string
+	var err error
+
+	if v == nil {
+		v = g.Views()[0]
+	}
+
+	_, cy := v.Cursor()
+	if l, err = v.Line(cy); err != nil {
+		l = ""
+	}
+
+	repo := strings.TrimLeft(strings.Split(l, "]")[0], "[")
+	url := "https://github.com/" + repo
+	return exec.Command("google-chrome", url).Run()
+}
+
+func cursorDown(g *gocui.Gui, v *gocui.View) error {
+	if v == nil {
+		v = g.Views()[0]
+	}
+
+	cx, cy := v.Cursor()
+	if err := v.SetCursor(cx, cy+1); err != nil {
+		ox, oy := v.Origin()
+		if err := v.SetOrigin(ox, oy+1); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func cursorUp(g *gocui.Gui, v *gocui.View) error {
+	if v == nil {
+		v = g.Views()[0]
+	}
+
+	ox, oy := v.Origin()
+	cx, cy := v.Cursor()
+	if err := v.SetCursor(cx, cy-1); err != nil && oy > 0 {
+		if err := v.SetOrigin(ox, oy-1); err != nil {
+			return err
+		}
+	}
 	return nil
 }
